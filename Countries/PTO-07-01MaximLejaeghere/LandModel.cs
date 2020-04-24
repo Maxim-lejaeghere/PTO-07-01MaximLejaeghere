3664a74def469be50f465829bd38a07fdcc8db0e
Maxim Lejaeghere <maxim.lejaeghere@contribute.be>
2020-04-24 12:19:46 +0200

    Made already long time ago but forgit to push

---
+++ Countries/PTO-07-01MaximLejaeghere/LandModel.cs
@@ -0,0 +1,288 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace PTO_07_01MaximLejaeghere
+{
+    class LandModel
+    {
+        //fields
+        private string _land;
+
+        public string Land
+        {
+            get { return _land; }
+            set { _land = value; }
+        }
+        
+        private string _stad;
+
+        public string Stad
+        {
+            get { return _stad; }
+            set { _stad = value; }
+        }
+
+       
+        private string _hoofdstad;
+
+        public string Hoofdstad
+        {
+            get { return _hoofdstad; }
+            set { _hoofdstad = value; }
+        }
+
+        private string _munteenheid;
+
+        public string Munteenheid
+        {
+            get { return _munteenheid; }
+            set { _munteenheid = value; }
+        }
+
+        private string _talen;
+
+        public string Talen
+        {
+            get { return _talen; }
+            set { _talen = value; }
+        }
+
+        private string _feestdag;
+
+        public string Feestdag
+        {
+            get { return _feestdag; }
+            set { _feestdag = value; }
+        }
+
+        private string _bezienswaardigheid;
+
+        public string Bezienswaardigheid
+        {
+            get { return _bezienswaardigheid; }
+            set { _bezienswaardigheid = value; }
+        }
+
+        private bool _bezocht;
+
+        public bool Bezocht
+        {
+            get { return _bezocht; }
+            set { _bezocht = value; }
+        }
+
+        private bool _checkBezocht;
+
+        public bool CheckBezocht
+        {
+            get { return _checkBezocht; }
+            set { _checkBezocht = value; }
+        }
+
+        private bool _fout;
+
+        public bool Fout
+        {
+            get { return _fout; }
+            set { _fout = value; }
+        }
+
+        private string _bezochtOfNiet;
+
+        public string BezochtOfNiet
+        {
+            get { return _bezochtOfNiet; }
+            set {_bezochtOfNiet = value; }
+        }
+
+        private List<LandModel> _bezienswaardigheidslijst = new List<LandModel>();
+
+        public List<LandModel> BezienwaardigheidLijst
+        {
+            get { return _bezienswaardigheidslijst; }
+            set { _bezienswaardigheidslijst = value; }
+        }
+
+        private List<string> _tempLijst = new List<string>();
+
+        public List<string> TempLijst
+        {
+            get { return _tempLijst; }
+            set { _tempLijst = value; }
+        }
+
+        private Color _kleur;
+
+        public Color Kleur
+        {
+            get { return _kleur; }
+            set { _kleur = value; }
+        }
+
+        //constructors
+
+        public LandModel(string land, string hoofdstad, string stad, string munteenheid, string talen, string feestdag, string bezienswaardigheid, bool bezocht)
+        {
+            Land = land;
+            Hoofdstad = hoofdstad;
+            Stad = stad;
+            Munteenheid = munteenheid;
+            Talen = talen;
+            Feestdag = feestdag;
+            Bezienswaardigheid = bezienswaardigheid;
+            Bezocht = bezocht;
+        }
+
+        //Methodes
+        
+
+        // Hier worden er al wat info in de lijst gestoken
+        public void InitiëleBezienswaardigheden()
+        {
+            BezienwaardigheidLijst.Add(new LandModel(Land = "België", Hoofdstad = "Brussel", Stad = "Eeklo", Munteenheid = "Euro", Talen = "Nederlands", Feestdag = "21 Juni", Bezienswaardigheid = "Kerk", Bezocht = true));
+            BezienwaardigheidLijst.Add(new LandModel(Land = "nederland", Hoofdstad = "Amsterdam", Stad = "Nijmwegen", Munteenheid = "Euro", Talen = "Nederlands", Feestdag = "02 Augustus", Bezienswaardigheid = "Tulpen", Bezocht = false));
+            BezienwaardigheidLijst.Add(new LandModel(Land = "België", Hoofdstad = "Brussel", Stad = "Brussel", Munteenheid = "Euro", Talen = "Nederlands", Feestdag = "21 Juni", Bezienswaardigheid = "Mannekenpis", Bezocht = true));
+            BezienwaardigheidLijst.Add(new LandModel(Land = "België", Hoofdstad = "Brussel", Stad = "Eeklo", Munteenheid = "Euro", Talen = "Frans", Feestdag = "21 Juni", Bezienswaardigheid = "Markt", Bezocht = false));
+        }
+
+
+        // Deze klasse zorgt er voor dat er een lijst wordt aangemaakt met alle verschillende soorten landen, maar een land kan hier geen 2 keer inzitten.
+        public void LandenAanComboToevoegen()
+        {
+            
+            TempLijst.Clear();
+            foreach (LandModel Nieuwebezienswaardigheid in BezienwaardigheidLijst)
+            {
+                Land = Nieuwebezienswaardigheid.Land.ToLower();
+                if (TempLijst.Contains(Land) == false)
+                {
+                        TempLijst.Add(Land);
+                }
+
+               
+            }
+        }
+
+       
+        public void StedenAanComboToevoegen(string geselecteerdLand)
+        {
+            TempLijst.Clear();
+            foreach (LandModel Nieuwebezienswaardigheid in BezienwaardigheidLijst)
+            {
+                // zorgt er voor dat er een lijst wordt aangemaakt met alle verschillende soorten steden, maar een stad kan hier geen 2 keer inzitten.
+                Stad = Nieuwebezienswaardigheid.Stad;
+                Land = Nieuwebezienswaardigheid.Land;
+                if (TempLijst.Contains(Stad) == false && Land == geselecteerdLand)
+                {
+                    TempLijst.Add(Stad);
+                }
+                // Hier worden de gegevens van het land toegevoegd in de variabelen, deze zullen we dan in de MainWindow gebruiken om de Labels in te vullen
+                if (Land == geselecteerdLand)
+                {
+                    Land = Nieuwebezienswaardigheid.Land;
+                    Hoofdstad = Nieuwebezienswaardigheid.Hoofdstad;
+                    Munteenheid = Nieuwebezienswaardigheid.Munteenheid;
+                    Talen = Nieuwebezienswaardigheid.Talen;
+                    Feestdag = Nieuwebezienswaardigheid.Feestdag;
+                }
+
+                
+            }
+
+            
+        }
+        // Deze klasse zorgt er voor dat er een lijst wordt aangemaakt met alle verschillende soorten Talen, maar een Taal kan hier geen 2 keer inzitten.
+        public void TalenToevoegen(string geselecteerdLand)
+        {
+            TempLijst.Clear();
+            foreach (LandModel Nieuwebezienswaardigheid in BezienwaardigheidLijst)
+            {
+                Talen = Nieuwebezienswaardigheid.Talen;
+                Land = Nieuwebezienswaardigheid.Land;
+
+                if (TempLijst.Contains(Talen) == false && Land == geselecteerdLand)
+                {
+                    TempLijst.Add(Nieuwebezienswaardigheid.Talen);
+                }
+            }
+        }
+
+        // Deze klasse zorgt er voor dat er een lijst wordt aangemaakt met alle verschillende soorten Bezienswaardigheiden, maar een bezienswaardigheid kan hier geen 2 keer inzitten.
+        public void BeziensAanComboToevoegen(string geselecteerdLand, string geselecteerdStad)
+        {
+            TempLijst.Clear();
+            foreach (LandModel Nieuwebezienswaardigheid in BezienwaardigheidLijst)
+            {
+                Stad = Nieuwebezienswaardigheid.Stad;
+                Land = Nieuwebezienswaardigheid.Land;
+
+                if (TempLijst.Contains(Stad) == false && Land == geselecteerdLand && Stad == geselecteerdStad)
+                {
+                    TempLijst.Add(Nieuwebezienswaardigheid.Bezienswaardigheid);
+                }
+            }
+        }
+
+        // Deze Klasse zorgt ervoor dat Alle info die is gelinkt aan een bezienswaardigheid,
+        //gelinkt worden met de juiste variablen, zodat deze in de Mainwindow kunnen worden gelinkt aan de Labels.
+        public void BeziensInfoToevoegen(string land, string stad, string bezienswaardigheid)
+        {
+            Kleur = Colors.AliceBlue;
+            TempLijst.Clear();
+            foreach (LandModel Nieuwebezienswaardigheid in BezienwaardigheidLijst)
+            {
+                Stad = Nieuwebezienswaardigheid.Stad;
+                Land = Nieuwebezienswaardigheid.Land;
+                Bezienswaardigheid = Nieuwebezienswaardigheid.Bezienswaardigheid;
+
+                if (Land == land && Stad == stad && Bezienswaardigheid == bezienswaardigheid)
+                {
+
+
+                    if (Nieuwebezienswaardigheid.Bezocht == true)
+                    {
+
+                        Kleur = Colors.Green;
+                        BezochtOfNiet = "BEZOCHT";
+                    }
+                    else
+                    {
+
+                        Kleur = Colors.Red;
+                        BezochtOfNiet = "NIET BEZOCHT";
+                    }
+                }
+
+            }
+
+        }
+
+        public void BezienswaardighedenToevoegen()
+        {
+
+            foreach (LandModel Nieuwebezienswaardigheid in BezienwaardigheidLijst)
+            {
+                if (Land.ToLower() == Nieuwebezienswaardigheid.Land.ToLower() && Hoofdstad.ToLower() == Nieuwebezienswaardigheid.Hoofdstad.ToLower() && Stad.ToLower() == Nieuwebezienswaardigheid.Stad.ToLower() && Munteenheid.ToLower() == Nieuwebezienswaardigheid.Munteenheid.ToLower() && Feestdag.ToLower() == Nieuwebezienswaardigheid.Feestdag.ToLower() && Talen.ToLower() == Nieuwebezienswaardigheid.Talen.ToLower() && Bezienswaardigheid.ToLower() == Nieuwebezienswaardigheid.Bezienswaardigheid.ToLower())
+                {
+
+                }
+
+                else if (Land == string.Empty || Hoofdstad == string.Empty || Stad == string.Empty || Munteenheid == string.Empty || Feestdag == string.Empty || Talen == string.Empty || Bezienswaardigheid == string.Empty)
+                {
+                    Fout = true;
+                }
+                else
+                {
+                    BezienwaardigheidLijst.Add(new LandModel(Land, Hoofdstad, Stad, Munteenheid, Talen, Feestdag, Bezienswaardigheid, Bezocht));
+                    LandenAanComboToevoegen();
+                    break;
+                }
+
+            }
+        }
+    }
+}
---
+++ Countries/PTO-07-01MaximLejaeghere/MainWindow.xaml.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PTO_07_01MaximLejaeghere
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+
+        // in de opdracht stond dat ik een listbox moest gebruiken voor de munteenheden,
+        // ik heb dit niet gedaan omdat volgens mij de andere listboxen duidelijk aantoonden dat ik wel genoeg kennis had. 
+        // En omdat ik het logischer vond op deze manier.
+       
+    {
+        LandModel bezienswaardigheid = new LandModel(string.Empty, string.Empty, string.Empty, string.Empty, string.Empty, string.Empty, string.Empty,  true );
+
+        public MainWindow()
+        {   
+            InitializeComponent();
+
+            // Standaard eerste landen toevoegen met een bezienswaardigheid in list
+            bezienswaardigheid.InitiëleBezienswaardigheden();
+
+
+            //Aanroepen verschillende landen 
+            bezienswaardigheid.LandenAanComboToevoegen();
+            foreach (string Land in bezienswaardigheid.TempLijst)
+            {
+                comboLand.Items.Add(Land);
+            }
+            
+
+
+        }
+
+        private void comboLand_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //Labels en comboBoxen Resetten
+            comboBezienswaardigheid.Items.Clear();
+            comboStad.Items.Clear();
+            comboTalen.Items.Clear();
+            lblBezocht.Content = "";
+            lblBezocht.Background = new SolidColorBrush(Colors.AliceBlue);
+
+            //Aanroepen verschillende steden 
+            bezienswaardigheid.StedenAanComboToevoegen(comboLand.SelectedItem.ToString());
+
+
+            foreach (string stad in bezienswaardigheid.TempLijst)
+            {
+                comboStad.Items.Add(stad);
+            }
+            //Alle Items die enkel gekoppeld zijn aan het land worden hier al getoond
+            lblFeestDag.Content = bezienswaardigheid.Feestdag;
+            lblHoofdStad.Content = bezienswaardigheid.Hoofdstad;
+            lblMunt.Content = bezienswaardigheid.Munteenheid;
+
+            //Veschillende Talen Toevoegen
+            bezienswaardigheid.TalenToevoegen(comboLand.SelectedItem.ToString());
+            foreach (string Taal in bezienswaardigheid.TempLijst)
+            {
+                comboTalen.Items.Add(Taal);
+            }
+        }
+
+        private void comboStad_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            // Labels resetten
+            lblBezocht.Content = "";
+            lblBezocht.Background = new SolidColorBrush(Colors.AliceBlue);
+            comboBezienswaardigheid.Items.Clear();
+
+            
+            //zorgt er voor dat de onderstaande berekening niet wordt uitgevoerd als deze SelectionChanged word getriggered door een andere SelectionChanged
+            if (comboStad.SelectedItem == null)
+            {
+
+            }
+            //Aanroepen verschillende steden 
+            else
+            {
+                bezienswaardigheid.BeziensAanComboToevoegen(comboLand.SelectedItem.ToString(), comboStad.SelectedItem.ToString());
+                foreach (string bezienswaardigheid in bezienswaardigheid.TempLijst)
+                {
+                    comboBezienswaardigheid.Items.Add(bezienswaardigheid);
+
+                }
+            }
+        }
+
+        private void comboBezienswaardigheid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //zorgt er voor dat de onderstaande berekening niet wordt uitgevoerd als deze SelectionChanged word getriggered door een andere SelectionChanged
+            if (comboStad.SelectedItem == null || comboBezienswaardigheid.SelectedItem == null)
+            {
+                
+            }
+
+            // Roept een lijst aan met alle Talen in Een land. Opgelet je kan maar 1 land aan 1 bezienswaardigheid koppelen,
+            //dus als je 2 talen wilt toevoegen en je hebt maar 1 bezienswaardigheid, zal je deze er 2 keer moeten insteken.
+            else
+            {
+                bezienswaardigheid.BeziensInfoToevoegen(comboLand.SelectedItem.ToString(), comboStad.SelectedItem.ToString(), comboBezienswaardigheid.SelectedItem.ToString());
+
+                foreach (string taal in bezienswaardigheid.TempLijst)
+                {
+                    comboTalen.Items.Add(taal);
+
+                }
+                lblBezocht.Background = new SolidColorBrush(bezienswaardigheid.Kleur);
+                lblBezocht.Content = bezienswaardigheid.BezochtOfNiet;
+            }
+        }
+
+        private void btnToevoegen_Click(object sender, RoutedEventArgs e)
+        {
+            bezienswaardigheid.Land = txtLand.Text;
+            bezienswaardigheid.Stad = txtStad.Text;
+            
+            if (checkBezocht.IsChecked == true)
+            {
+                bezienswaardigheid.Bezocht = true;
+            }
+            else
+            {
+                bezienswaardigheid.Bezocht = false;
+            }
+            bezienswaardigheid.Feestdag = txtFeestdag.Text;
+            bezienswaardigheid.Hoofdstad = txtHoofdstad.Text;
+            bezienswaardigheid.Talen = txtTaal.Text;
+            bezienswaardigheid.Munteenheid = txtMunteenheid.Text;
+            bezienswaardigheid.Bezienswaardigheid = txtBezienswaardigheid.Text;
+
+            //
+            bezienswaardigheid.BezienswaardighedenToevoegen();
+
+            // Standaard extra landen toevoegen met een bezienswaardigheid in list
+            bezienswaardigheid.InitiëleBezienswaardigheden();
+
+            //Aanroepen verschillende landen 
+            bezienswaardigheid.LandenAanComboToevoegen();
+            comboLand.Items.Clear();
+            foreach (string Land in bezienswaardigheid.TempLijst)
+            {
+                comboLand.Items.Add(Land);
+            }
+
+
+
+            if (bezienswaardigheid.Fout == true)
+            {
+                lblFout.Content = "Vul alles Goed in!";
+            }
+            else
+            {
+                txtLand.Text = string.Empty;
+                txtStad.Text = string.Empty;
+                txtFeestdag.Text = string.Empty;
+                txtFeestdag.Text = string.Empty;
+                txtHoofdstad.Text = string.Empty;
+                txtTaal.Text = string.Empty;
+                txtMunteenheid.Text = string.Empty;
+                checkBezocht.IsChecked = false;
+                txtBezienswaardigheid.Text = string.Empty;
+                lblFout.Content = string.Empty;
+            }
+        }
+
+        private void btnAfsluiten_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
